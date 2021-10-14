5ec03f2a0406558b55bccd8feb7ff30e1b36daec
Name Ping Louie <namepinglouie@gmail.com>
2021-10-14 15:06:44 -0400

    fixed issue on displaying title, year, and description

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let selection = document.querySelector("#titles");
+let selection = document.querySelector("select");
 let headerSection = document.querySelector("header");
+let displayContent = document.querySelector("#content");
+
 let img = document.createElement("img");
 img.src = "./images/ghibli-logo.png";
 headerSection.append(img);
@@ -10,10 +12,36 @@ fetch(url)
     .then(result => result.json())
     .then((data) => {
         let dataList = data;
-        for(let data of dataList) {
+
+        for(let d of dataList) {
+            //console.log(d);
             let option = document.createElement("option");
-            option.value = data.title.toLowerCase().split(" ").join("-");
-            option.textContent = data.title;
-            selection.append(option);
+            option.value = d.title;
+            option.textContent = d.title;
+            selection.append(option);   
         }
+
+        selection.addEventListener("change", (e)=> {
+            for(let d of dataList) { 
+                if(selection.value === d.title) {
+                    let title = d.title;
+                    let year = d.release_date;
+                    let desc = d.description;
+
+                    displayContent.innerHTML = 
+                    `
+                    <h3>${title}</h3>
+                    <p>${year}</p>
+                    <p>${desc}</p>
+                    `
+                }
+            }
+
+
+
+            
+        })
     })
+    .catch((error) => console.log(error));
+
+   
